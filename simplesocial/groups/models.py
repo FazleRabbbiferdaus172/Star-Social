484270778b80612aa724021e786aa54d204fae44
FazleRabbiFerdaus172 <fazle.ferdaus1416@gmail.com>
2021-06-03 02:43:27 +0600

    added posts model

--- simplesocial/groups/models.py
+++ simplesocial/groups/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.utils.text import slugify
 from django.contrib.auth import get_user_model
 import misaka
-from django.urls import reverse
+from django.core.urlresolvers import reverse
 # Create your models here.
 
 User = get_user_model()
